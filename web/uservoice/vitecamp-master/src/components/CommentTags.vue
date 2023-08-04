d463fa6ddcec8d8ca30fc341d66d9de292949cd9
HeHong <619215399@qq.com>
2023-08-04 22:42:46 +0800

    Init

---
+++ web/uservoice/vitecamp-master/src/components/CommentTags.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-radio-group v-model="tag" size="large">
+    <el-radio-button label="推荐" />
+    <el-radio-button label="最新" />
+    <el-radio-button label="好问题" />
+  </el-radio-group>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const tag = ref('推荐');
+</script>
---
+++ web/uservoice/vitecamp-master/src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container w-full mx-auto">
+    <div class="mx-auto w-200">
+      <el-card class="box-card">
+        <CommentTags />
+      </el-card>
+      <div class="m-5"></div>
+      <el-card class="box-card">
+        <Comment />
+      </el-card>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss" scoped></style>
